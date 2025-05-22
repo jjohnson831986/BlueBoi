04929239a7cbca594ec344535d3eb42b8c35afdb
jjohnson831986 <jjohnson831986@gmail.com>
2025-05-22 09:32:16 -0500

    Added game loop timers, basic player object, and movement functionality.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,6 +2,8 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -17,12 +19,23 @@ public class GamePanel extends JPanel implements Runnable {
 	final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL; // 768px
 	final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW; // 576px
 	
+	// FPS
+	int FPS = 60;
+	
+	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
+	
+	// Set players default position
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4; // pixels per movement
 	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
 	}
 	
 	public void startGameThread() {
@@ -32,7 +45,47 @@ public class GamePanel extends JPanel implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO work on loop
+		double drawInterval = 1000000000/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		
+		while(gameThread != null) {			
+			currentTime = System.nanoTime();
+			delta += (currentTime - lastTime)/drawInterval;
+			lastTime = currentTime;
+			
+			if(delta >= 1) {
+				update();
+				repaint();
+				delta--;
+			}
+		}
+	}
+	
+	public void update() {
+		if(keyH.upPressed == true) {
+			playerY -= playerSpeed;
+		}
+		else if(keyH.downPressed == true) {
+			playerY += playerSpeed;
+		}
+		else if(keyH.leftPressed == true) {
+			playerX -= playerSpeed;
+		}
+		else if(keyH.rightPressed == true) {
+			playerX += playerSpeed;
+		}
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		// Player Character object
+		Graphics2D g2 = (Graphics2D)g;		
+		g2.setColor(Color.white);
+		g2.fillRect(playerX, playerY, TILE_SIZE, TILE_SIZE);
+		g2.dispose();
 	}
 
 }
